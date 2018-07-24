0fb8c5404051106231f7e3e57907a02b75fda710
Adriana <adygranger@live.com.mx>
2018-07-24 12:56:42 -0500

    estoy guardadndo los archvos ejecutables

---
+++ s1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Inicia el scrpt 1"
+ls
+mkdir magia
+touch magia/alibaba
+echos "focus pocua" < magia/alibaba
+cat magia/alibaba
+echo "Fin del scrpt 1"
+
---
+++ s2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "inicia el script 2"
+ls
+./s1.sh
